e8d0beaff387969bf9717fef30781cbf46057cc4
Marwan Alshaker <7kami12@gmail.com>
2022-05-31 23:49:31 +0200

    Remove unused code

--- Components/ErrorPage/ErrorPage.js
+++ Components/ErrorPage/ErrorPage.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import styles from './ErrorPage.module.css';
-import {
-  SignalWifiConnectedNoInternet4TwoTone,
-  WarningOutlined,
-} from '@mui/icons-material';
-import TopBar from '../Topbar/Topbar';
+import { SignalWifiConnectedNoInternet4TwoTone } from '@mui/icons-material';
 
 export default function ErrorPage() {
   return (
